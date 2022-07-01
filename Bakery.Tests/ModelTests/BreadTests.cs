0ac6358035aa7f048487c539a4abc7055561663e
RosarioRu <mariarruval@gmail.com>
2022-07-01 12:55:52 -0700

    Add test files with basic 'boilerplate' code for testing Bread and Pastry classes.

---
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bakery;
+
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class BreadTests
+  {
+    // Test methods for Bread go here
+  }
+
+  [TestClass]
+  public class PastryTests
+  {
+    //test methods for pastries go here
+  }
+}
