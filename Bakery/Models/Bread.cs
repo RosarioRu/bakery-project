9c4b32f88c913d949876f5d3dc68e854bcce3dcc
RosarioRu <mariarruval@gmail.com>
2022-07-01 11:28:17 -1000

    Write constructor for creating instances of Bread.

--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -2,6 +2,11 @@ namespace Bakery.Models
 {
   public class Bread
   {
+    //constructor
+    public Bread(int loavesOfBread)
+    {
+      LoavesOfBread = loavesOfBread;
+    }
     //properties, constructors, methods, etc. 
   }
 }
