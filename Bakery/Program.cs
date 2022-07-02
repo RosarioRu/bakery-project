1aba98916fd0f34ae88ec3dfc2a054cf17fbddb8
RosarioRu <mariarruval@gmail.com>
2022-07-01 21:49:52 -1000

    Add basic class and namespace to program file.

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -1,3 +1,14 @@
 using Bakery.Models;
 using System;
 using System.Collections.Generic;
+
+namespace Bakery
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      //write things here
+    }
+  }
+}
