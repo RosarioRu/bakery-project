8b1cd8a81333786e5f8784c54fb2f9d2fb401dd9
RosarioRu <mariarruval@gmail.com>
2022-07-01 18:28:32 -1000

    Pass first test.

--- Bakery/Models/Pastry.cs
+++ Bakery/Models/Pastry.cs
@@ -2,6 +2,35 @@ namespace Bakery.Models
 {
   public class Pastry
   {
+    //properties
+
+    //constructor
+
+    //public methods
+
+    //private methods
+
+
+    // properties
+    // public int LoavesOfBread {get;}
+
+    // constructor
+    // public Bread(int loavesOfBread)
+    // {
+    //   LoavesOfBread = loavesOfBread;
+    // }
+
+    // public methods
+    // public int DisplayBreadTotal()
+    // {
+    //   return CostOfBread();
+    // }
+
+    // private methods
+    // private int CostOfBread()
+    // {
+    //   return LoavesOfBread * 5 - (LoavesOfBread/3*5);    
+    // }
     //properties, constructors, methods, etc. 
   }
 }
