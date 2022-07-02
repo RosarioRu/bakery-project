2690badcd0d51a79862d1e77c86b3b1db6526254
RosarioRu <mariarruval@gmail.com>
2022-07-01 22:34:50 -1000

    Add code to print requested loaves/croissants to user.

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -8,19 +8,24 @@ namespace Bakery
   {
     public static void Main()
     {
+      Console.WriteLine(" ");
       Console.WriteLine("WELCOME TO PIERRE'S BAKERY!");
       Console.WriteLine("Today's daily loaf is our Seeded Sourdough. For pastry we have Pistachio Croissant.");
-      Console.WriteLine("----------------------------");
-      Console.WriteLine("Seeded Sourdough: $5/loaf. Buy 2, get 1 free!")
-      Console.WriteLine("Pistachio Croissant: 1 for $2, 3 for $5, 4 for $7, 5 for $9, and a half dozen for $10")
-
+      Console.WriteLine(" ");
+      Console.WriteLine("----------------------------------------------");
+      Console.WriteLine("Seeded Sourdough: $5/loaf. Buy 2, get 1 free!");
+      Console.WriteLine("Pistachio Croissant: 1 for $2, 3 for $5, 4 for $7, 5 for $9, and a half dozen for $10");
+      Console.WriteLine("----------------------------------------------");
       Console.WriteLine("How Many loaves of bread would you like to order?");
       int requestedLoaves = int.Parse(Console.ReadLine());
-
       Console.WriteLine("How many croissants would you like to order?");
       int requestedPastries = int.Parse(Console.ReadLine());
+      Console.WriteLine(" ");
+      Console.WriteLine("You have requested " + requestedLoaves + " loaves of bread and " + requestedPastries + " croissants."); 
 
       
+
+
     }
   }
 }
