efc3cb1574e22dc9663ab511ced043dbf328ed48
RosarioRu <mariarruval@gmail.com>
2022-07-01 18:10:29 -1000

    Update code in CostOfBread() to correctly calculate the cost of 3 loaves of braed.

--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -22,7 +22,8 @@ namespace Bakery.Models
     //private methods
     private int CostOfBread()
     {
-      return LoavesOfBread*5;    
+      
+      return LoavesOfBread * 5 - (LoavesOfBread/3*5);    
     }
     //properties, constructors, methods, etc. 
   }
