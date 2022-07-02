459fd4efcf5a731e1418887ed15c60202469c6cb
RosarioRu <mariarruval@gmail.com>
2022-07-02 08:18:16 -1000

    WIP: still trying to filter our non numeric answers.

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -16,17 +16,39 @@ namespace Bakery
       Console.WriteLine("Seeded Sourdough: $5/loaf. Buy 2, get 1 free!");
       Console.WriteLine("Pistachio Croissant: 1 for $2, 3 for $5, 4 for $7, 5 for $9, and a half dozen for $10");
       Console.WriteLine("----------------------------------------------");
+
+      
       Console.WriteLine("How Many loaves of bread would you like to order?");
 
       string breadAnswer = Console.ReadLine();
       int requestedLoaves;
       bool checkBreadAnswer = int.TryParse(breadAnswer, out requestedLoaves);
+
+      if (checkBreadAnswer == false)
+      {
+        Console.WriteLine("Please re-enter your answer as a number.");
+        Main();
+      }
+      else
+      {
+      Console.WriteLine("How many croissants would you like to order?");
+      string pastryAnswer = Console.ReadLine();
+      int requestedPastries;
+      bool checkPastryAnser = int.TryParse(pastryAnswer, out requestedPastries);
+      // } 
+      // else
+      // {
+      //   Console.WriteLine("Please enter your answer as a number.");
+      //   Main();
+      // }
+
       Console.WriteLine("checking bool " + checkBreadAnswer);
+      Console.WriteLine("checking requestedLoaves " + requestedLoaves);
+      Console.WriteLine("checking requestedPastries " + requestedPastries);
 
       // int requestedLoaves = int.Parse(Console.ReadLine());
-      //error handling for non-numberic values
-      Console.WriteLine("How many croissants would you like to order?");
-      int requestedPastries = int.Parse(Console.ReadLine());
+      // Console.WriteLine("How many croissants would you like to order?");
+      // int requestedPastries = int.Parse(Console.ReadLine());
       Console.WriteLine(" ");
       Console.WriteLine("You have requested " + requestedLoaves + " loaves of bread and " + requestedPastries + " croissants."); 
 
@@ -36,6 +58,7 @@ namespace Bakery
       int totalCostOfPastry = pastryOrder.DisplayPastryTotal();
       int orderTotal = toalCostOfBread + totalCostOfPastry;
       Console.WriteLine("Your total is " + "$" + orderTotal + ".");
+    }
     }
   }
 }
