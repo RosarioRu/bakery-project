95d806db57a1f075ef1cded0e35db683cfd4b2be
RosarioRu <mariarruval@gmail.com>
2022-07-02 23:03:47 -1000

    Change name of function from GetBreadTotal() to GetBreadAndPastryTotal so it is more accurate.

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -16,10 +16,11 @@ namespace Bakery
       Console.WriteLine("Pistachio Croissant: 1 for $2, 3 for $5, 4 for $7, 5 for $9, and a half dozen for $10");
       Console.WriteLine("----------------------------------------------");
 
-      GetBreadOrder();
+      GetBreadAndPastryTotal();
     }
 
-    public static void GetBreadOrder()
+    public static void GetBreadAndPastryTotal
+    ()
     {
       Console.WriteLine("How Many loaves of bread would you like to order?");
       string breadAnswer = Console.ReadLine();
@@ -28,7 +29,7 @@ namespace Bakery
       if (checkBreadAnswer == false)
       {
         Console.WriteLine("Please re-enter your answer as a number.");
-        GetBreadOrder();
+        GetBreadAndPastryTotal();
       }
       else
       {
